bd5dfa2abac9550cfd880a2ed45c5e328e775d17
megn@example.com <megn@example.com>
2021-02-21 04:23:04 -0800

    added style to one element.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('btn1').onclick = function() {
   document.getElementById('notif').innerHTML = "your input is: " + document.getElementById('inp1').value 
+  document.getElementById('notif').style.color = '#0094d6'
 }
 
 document.getElementById('btn2').onclick = function() {
